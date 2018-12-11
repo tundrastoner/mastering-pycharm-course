a123dfa5d8bc63929759b316b4a9abc95f175da3
tundrastoner <13054025+tundrastoner@users.noreply.github.com>
2018-12-11 09:56:25 -0900

    Committed but npt pushed

--- demos/podcast/program.py
+++ demos/podcast/program.py
@@ -1,16 +1,25 @@
 import service
 
+
 def main():
-    print("Welcome to the talk python info downloader.")
-    print()
+
+    show_header()
 
     service.download_info()
 
+    display_results()
+
+
+
+def show_header():
+    print("Welcome to the talk python info downloader.")
+    print()
+
+def display_results():
     for show_id in range(120, 140):
         info = service.get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
 
-    print("Foo")
 
 if __name__ == '__main__':
     main()
